7c2cdce1c0f5683ba78dbbc32ed3cfce4413167f
Maxime Faure <maximejfaure@gmail.com>
2017-04-12 17:26:15 +0200

    Ajout de la premiere release

---
+++ App.java
@@ -0,0 +1,16 @@
+package td;
+
+public class App 
+ {
+    public static int max(int a, int b)
+    {	
+    	//Version stagiaire :
+   	return a;
+    }
+	
+     public static void main( String[] args )
+     {
+    	int result = max(4, 5);
+        System.out.println(result);
+     }
+ }
---
+++ AppTest.java
@@ -0,0 +1,7 @@
+	public void testApp()
+     {
+       		App var = new App();
+        	assertEquals(5, var.max(4, 5));
+       		assertEquals(5, var.max(5, 4));
+     }
+ }
